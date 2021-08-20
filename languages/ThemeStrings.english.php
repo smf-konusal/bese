1b1a814f5c4189c101c61e4260fb281915188fee
CeeMoo <qdale.123@gmail.com>
2021-08-20 18:54:17 +0300

    Initial commit

---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,19 @@
+<?php
+
+$txt['themecopyright'] = ' <strong>Bese Theme By Huu</strong>';
+
+
+$txt['bese_color_default'] = 'bese_color_default';
+$txt['bese_color_primary'] = 'bese_color_primary';
+$txt['bese_color_secondary'] = 'bese_color_secondary';
+$txt['bese_default_mode'] = 'bese_default_mode';
+
+$txt['bese_color_defaulte'] = 'Default is: %color%';
+
+
+$txt['bese_default_mode'] = 'Default Mode';
+$txt['bese_dark_mode'] = 'Dark Mode';
+$txt['bese_light_mode'] = 'Light Mode';
+$txt['bese_theme_settings'] = 'Theme Settings';
+
+?>
